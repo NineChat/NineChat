68c5317c9056e8739e0129bc2d56da9ca9c5b219
Garrett Levy <garrettlr@gmail.com>
2017-07-06 18:21:27 -0700

    added webpack dependencies to package.json, built out webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './'
+  },
+  entry: [
+    './client/index.js',
+    './client/styles.css',
+  ],
+
+  output: {
+    path: path.resolve(__dirname, '/build'),
+    publicPath: '/build/',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /(\.js|\.jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/,
+        query: {
+          presets: ['es2015', 'react']
+        },
+      },
+      {
+        test: /(\.css|.scss)$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+    ]
+  },
+
+}
